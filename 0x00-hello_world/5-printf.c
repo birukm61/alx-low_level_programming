3d99af34a0b5e0400e251108aca333fb0118a16a
birukm61 <85063711+birukm61@users.noreply.github.com>
2021-06-17 13:16:28 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* 
-* main - project hello world task 5 
-*/
+
+/**
+ * main - Entry point
+ *
+ * prinf: print the characters given
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
