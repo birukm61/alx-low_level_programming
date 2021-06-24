3bd51d9beeed0d8bc47ca25e4f283f7a8f0264af
birukm61 <shegerbros@gmail.com>
2021-06-24 07:52:21 -0700

    Adding isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_isupper - checks if the char is upper case or ot 
+ *@c: character
+ *return: 0 or 1
+ */
+
+int_isupper(int c);
+{
+  if ( c >= 65 && c <= 90 )
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
