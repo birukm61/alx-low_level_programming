c4e75afbfd4520661ccc30bdebfc2fd257748d29
birukm61 <shegerbros@gmail.com>
2021-06-28 05:15:21 -0700

    adding file holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
-#ifndef HOLBETON_H
-#define HOLBERTON_H
+#ifndef HOLBERTON
+#define HOLBERTON
 
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,5 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 
 #endif
