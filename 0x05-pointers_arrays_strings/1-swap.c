405472062370e1f58fa4152897500fd8f75d7ae7
birukm61 <shegerbros@gmail.com>
2021-06-28 04:38:17 -0700

    adding file 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ * swap_int - swap value a with b
+ * @a - takes the value of b
+ * @b - takes the value of a
+ */
+void swap_int(int *a, int *b)
+{
+  const int p = *a
+  *a = *b
+  *b = p
+}    
+ 
