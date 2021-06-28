798229634bbdf4830b766e24f945757546f16476
birukm61 <shegerbros@gmail.com>
2021-06-28 04:13:33 -0700

    adding file 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
- * reset_to_98 - resting to 98
- * Return: Always 0.
- *  *n - value to be reset
+ *reset_to_98 - reasign value in address memory
+ *@n: parameter enteger
+ *Return: void
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-    int n;
-    int n=&n;
-    n = 402;
-    printf("n=%d\n", n);
-    *n = 98;
-    printf("n=%d\n", n);
-    return (0);
+	*n = 98;
 }
