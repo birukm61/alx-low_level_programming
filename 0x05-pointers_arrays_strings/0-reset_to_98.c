41718aebaa425838dd43609596bb8ea0d8ad12fe
birukm61 <shegerbros@gmail.com>
2021-06-28 03:52:34 -0700

    adding file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ * reset_to_98 - resting to 98
+ * Return: Always 0.
+ *  *n - value to be reset
+ */
+void reset_to_98(int *n);
+{
+    int n;
+    int n=&n;
+    n = 402;
+    printf("n=%d\n", n);
+    *n = 98;
+    printf("n=%d\n", n);
+    return (0);
+}
